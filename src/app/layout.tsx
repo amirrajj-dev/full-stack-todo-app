197c1566056e1976a9ed60f2d5d190f2a422ba5d
amirrajj-dev <amiramraja@gmail.com>
2025-01-21 00:08:29 +0330

    signin form validation completed🐟💙

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono , Roboto } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Toaster } from "@/components/ui/toaster";
 
 const RobotoF = Roboto({
   variable: "--font-roboto",
@@ -41,6 +32,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster  />
         </ThemeProvider>
       </body>
     </html>
--- src/components/SignInForm.tsx
+++ src/components/SignInForm.tsx
@@ -1,12 +1,50 @@
-import React from "react";
+'use client'
+import React, { useEffect } from "react";
 import { FiMail, FiLock } from "react-icons/fi";
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
 import { FaCheckDouble } from "react-icons/fa6";
-import Link from "next/link"; // Import Link for navigation
+import Link from "next/link";
+import { SignInSchemaType, schema } from '@/validations/signin.validation';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useToast } from "@/hooks/use-toast";
 
 const SignInForm = () => {
+  const { toast } = useToast();
+  const { register, handleSubmit, formState: { errors , isSubmitting }} = useForm<SignInSchemaType>({
+    resolver: zodResolver(schema)
+  });
+
+  const onSubmit = async (data) => {
+    console.log(data);
+  };
+
+  useEffect(() => {
+    if (errors.email) {
+      toast({
+        title: "Email Error",
+        description: errors.email.message,
+        variant: "destructive",
+        duration: 4000,
+      });
+    }
+
+    if (errors.password) {
+      toast({
+        title: "Password Error",
+        description: errors.password.message,
+        variant: "destructive",
+        duration: 4000,
+      });
+    }
+  }, [errors, toast]);
+
+  const handleFormSubmit = async (data) => {
+    console.log(data);
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-900 roboto px-2">
       <Card className="w-full max-w-sm bg-white dark:bg-gray-800 p-8 shadow-md rounded-lg">
@@ -17,11 +55,13 @@ const SignInForm = () => {
         <h2 className="text-2xl font-semibold mb-6 text-center text-gray-900 dark:text-gray-100">
           Sign In
         </h2>
-        <form className="space-y-4">
+        <form className="space-y-4" onSubmit={handleSubmit(handleFormSubmit)}>
           <div className="flex items-center border-b border-gray-300 dark:border-gray-600 py-2">
             <FiMail className="text-gray-400 mr-3" size={20} />
             <Input
+              {...register('email')}
               type="email"
+              name="email"
               placeholder="Email"
               className="w-full py-2 px-3 text-gray-700 dark:text-gray-300 bg-transparent focus:outline-none"
             />
@@ -29,7 +69,9 @@ const SignInForm = () => {
           <div className="flex items-center border-b border-gray-300 dark:border-gray-600 py-2">
             <FiLock className="text-gray-400 mr-3" size={20} />
             <Input
+              {...register('password')}
               type="password"
+              name="password"
               placeholder="Password"
               className="w-full py-2 px-3 text-gray-700 dark:text-gray-300 bg-transparent focus:outline-none"
             />
@@ -38,7 +80,7 @@ const SignInForm = () => {
             type="submit"
             className="w-full bg-emerald-600 text-white py-2 rounded-md hover:bg-emerald-700"
           >
-            Sign In
+            {isSubmitting? 'Signinig In ...' : 'Sign In'}
           </Button>
         </form>
         <div className="mt-6 text-center">
