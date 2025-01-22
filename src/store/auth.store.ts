4a102e0503cc05e6d0af9a121770f7e706c4c98f
amirrajj-dev <amiramraja@gmail.com>
2025-01-23 02:43:16 +0330

    big changes in auth store for better behaviour🐟💙

--- src/store/auth.store.ts
+++ src/store/auth.store.ts
@@ -1,7 +1,8 @@
+import { getCurrentUserAction, logOutAction, signInAction, signUpAction } from '@/actions/auth.action';
+import { toast } from '@/hooks/use-toast';
+import { SignUpType } from '@/validations/signup.validation';
 import { Priority, Status } from '@prisma/client';
-import { boolean, number } from 'zod'
-import {create} from 'zustand'
-
+import { create } from 'zustand'
 
 interface Todo {
     id: number;
@@ -15,16 +16,85 @@ interface Todo {
 interface User {
     name: string;
     email: string;
-    isLoggedIn: boolean;
-    todos? : Todo[]
+    password: string;
+    todos?: Todo[]
 }
 
-interface UserState {
+interface AuthStore {
     user: User | null;
     setUser: (user: User | null) => void;
+    getUser: () => void;
+    isLoading: boolean;
+    signup : (formdata : SignUpType)=>void;
+    logout : ()=>void;
+    login : (formdata : Pick<User , 'email' | 'password'>)=>void;
 }
 
-export const useAuth = create<UserState>((set) => ({
+export const useAuth = create<AuthStore>((set) => ({
     user: null,
-    setUser: (user) => set({user }),
+    isLoading: false,
+    setUser: (user) => set({ user }),
+    async getUser() {
+        set({ isLoading: true })
+        const res = await getCurrentUserAction()
+        if (res?.user) {
+            set({ user: res.user, isLoading: false })
+        } else {
+            set({ user: null, isLoading: false })
+        }
+    },
+    signup : async (formdata)=>{
+        set({ isLoading: true })
+        const res = await signUpAction(formdata)
+        if (res.success) {
+            set({ user: res.user })
+            toast({
+                title: res.message,
+                className: 'bg-emerald-600'
+            })
+            return {
+                success: true
+            }
+        }else{
+            set({ isLoading: false })
+            toast({
+                title : res?.message,
+                variant : 'destructive'
+            })
+            return {
+                success: false
+            }
+        }
+    },
+    logout: async () => {
+        const res = await logOutAction()
+        if (res.success){
+            set({ user: null })
+            toast({
+                title: res.message,
+                className: 'bg-emerald-600'
+            })
+        }
+    },
+    login: async (formdata) => {
+        const res = await signInAction(formdata)
+        if (res.success){
+            set({ user: res.user })
+            toast({
+                title : res.message,
+                className : 'bg-emerald-600'
+            })
+            return {
+                success: true
+            }
+        }else{
+            toast({
+                title : res?.message,
+                variant : 'destructive'
+            })
+            return {
+                success: false
+            }
+        }
+    }
 }))
