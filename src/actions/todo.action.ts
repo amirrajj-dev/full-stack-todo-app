7c261861e7b0403e85edbe1a8ad3805bd21a2988
amirrajj-dev <amiramraja@gmail.com>
2025-01-24 17:25:26 +0330

    create Todo Action Completed🐟💙

---
+++ src/actions/todo.action.ts
@@ -0,0 +1,61 @@
+'use server'
+import prisma from "../../utils/prisma";
+import { Prisma } from "@prisma/client";
+import { getCurrentUserAction } from "./auth.action";
+import { revalidatePath } from "next/cache";
+
+export const createTodoAction = async (todo: Pick<Prisma.TodoCreateInput , 'title' | 'priority'>) => {
+  try {
+    const currentUser = (await getCurrentUserAction())?.user;
+    if (!currentUser) {
+      throw new Error("User not authenticated");
+    }
+
+    const isTodoTitleExistInCurrentUserTodos = await prisma.todo.findFirst({
+      where: {
+        userId: currentUser.id,
+        title: todo.title,
+      },
+    });
+    
+    if (isTodoTitleExistInCurrentUserTodos) {
+     return {
+       success: false,
+       message: "Todo with this Title already exists in your todos",
+     }
+    }
+
+    // Create todo for the current user
+    const newTodo = await prisma.todo.create({
+      data: {
+        ...todo,
+        status : 'PENDING',
+        completed : false,
+        user: { connect: { id: currentUser.id } },
+      },
+    });
+
+    if (!newTodo) {
+        return { 
+          success: false,
+          message: "Failed to create todo, try again later",
+        }
+    }
+
+    
+    revalidatePath('/')
+
+    return {
+      success: true,
+      todo: newTodo,
+      message: "Todo created successfully",
+    };
+  } catch (error) {
+    console.error("Error creating todo:", error);
+    return {
+      success: false,
+      message: "An error occurred while creating the todo",
+      error,
+    };
+  }
+};
