2b3958351380f40fd83ba9d9c8dbcd992442eb9d
amirrajj-dev <amiramraja@gmail.com>
2025-01-26 21:33:37 +0330

    todo store completed🐟💙

---
+++ src/store/todo.store.ts
@@ -0,0 +1,116 @@
+import {create} from 'zustand';
+import { Todo, Priority, Status, Prisma } from '@prisma/client';
+import { createTodoAction, getTodosAction, updateTodoAction, deleteTodoAction } from '@/actions/todo.action';
+import { toast } from '@/hooks/use-toast';
+
+interface TodoStoreState {
+  todos: Todo[];
+  loading: boolean;
+  fetchTodos: () => Promise<void>;
+  createTodo: (title: string, priority: Priority) => Promise<void>;
+  updateTodo: (id: number, updatedTodo: Partial<Prisma.TodoCreateInput>) => Promise<void>;
+  deleteTodo: (id: number) => Promise<void>;
+  setPriority: (id: number, priority: Priority) => Promise<void>;
+  setStatus: (id: number, status: Status) => Promise<void>;
+}
+
+const useTodoStore = create<TodoStoreState>(
+    (set, get) => ({
+      todos: [],
+      loading: false,
+      fetchTodos: async () => {
+        set({ loading: true });
+        const res = await getTodosAction();
+        if (res.success) {
+          set({
+            todos: res.todos!.sort((a, b) => {
+              if (a.completed && !b.completed) return 1;
+              if (!a.completed && b.completed) return -1;
+              if (a.priority === 'HIGH' && b.priority !== 'HIGH') return -1;
+              if (a.priority !== 'HIGH' && b.priority === 'HIGH') return 1;
+              return 0;
+            }),
+          });
+        }
+        set({ loading: false });
+      },
+      createTodo: async (title: string, priority: Priority) => {
+        const res = await createTodoAction({ title, priority });
+        if (res.success) {
+          toast({
+            title: res.message,
+            className: 'bg-emerald-600',
+          });
+          get().fetchTodos();
+        } else {
+          toast({
+            title: res.message,
+            variant: 'destructive',
+          });
+        }
+      },
+      updateTodo: async (id: number, updatedTodo: Partial<Prisma.TodoCreateInput>) => {
+        const res = await updateTodoAction(id, updatedTodo);
+        if (res.success) {
+          toast({
+            title: res.message,
+            className: 'bg-emerald-600',
+          });
+          get().fetchTodos();
+        } else {
+          toast({
+            title: res.message,
+            variant: 'destructive',
+          });
+        }
+      },
+      deleteTodo: async (id: number) => {
+        const res = await deleteTodoAction(id);
+        if (res.success) {
+          toast({
+            title: res.message,
+            className: 'bg-emerald-600',
+          });
+          get().fetchTodos();
+        } else {
+          toast({
+            title: res.message,
+            variant: 'destructive',
+          });
+        }
+      },
+      setPriority: async (id: number, priority: Priority) => {
+        const res = await updateTodoAction(id, { priority });
+        if (res.success) {
+          toast({
+            title: res.message,
+            className: 'bg-emerald-600',
+          });
+          get().fetchTodos();
+        } else {
+          toast({
+            title: res.message,
+            variant: 'destructive',
+          });
+        }
+      },
+      setStatus: async (id: number, status: Status) => {
+        const res = await updateTodoAction(id, { status });
+        if (res.success) {
+          toast({
+            title: res.message,
+            className: 'bg-emerald-600',
+          });
+          get().fetchTodos();
+        } else {
+          toast({
+            title: res.message,
+            variant: 'destructive',
+          });
+        }
+      },
+    }),
+   
+  )
+
+export default useTodoStore;
